e5e85eed2406bb111d16e8e4e3343b54044bd570
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2021-04-22 16:30:20 -0300

    Service layer, compontent registration

--- src/main/java/com/eaprendendoweb/course/repositories/UserRepository.java
+++ src/main/java/com/eaprendendoweb/course/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.eaprendendoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.eaprendendoweb.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
