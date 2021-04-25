26c5848ea447b1ea17b5098de4e77db132e24d86
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2021-04-25 14:38:37 -0300

    Exceção de deleção do usuario

--- src/main/java/com/eaprendendoweb/course/services/UserService.java
+++ src/main/java/com/eaprendendoweb/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.eaprendendoweb.course.entities.User;
 import com.eaprendendoweb.course.repositories.UserRepository;
+import com.eaprendendoweb.course.services.exceptions.DatabaseException;
 import com.eaprendendoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
